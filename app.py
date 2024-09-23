e170fdec9fb08955d92ac5f89cfc681f90c89326
UMAR0800 <148867003+UMAR0800@users.noreply.github.com>
2024-09-23 12:54:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    print(hello())
-# app.py
-def hello():
-    return "Hello, World!"
-
-if __name__ == "__main__":
-    print(hello())
+    app.run(host="0.0.0.0", port=80)
